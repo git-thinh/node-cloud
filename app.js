aef2094b43bd485711f00a370c88c8b7d594c7a6
Mr Thinh <33323583+git-thinh@users.noreply.github.com>
2020-11-12 03:25:45 +0700

    333

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
-let ENV = 'dev'; //pro
+let ENV = 'pro';
+if (process.platform === "win32") ENV = 'dev';
 
 const DIR_PUBLISH = '_pub';
 
+const URL_STORE_DIR = {};
+let URL_CACHE_TEXT = {}
 let SITE = require('./_sys/domain.' + ENV + '.json');
 //console.log(SITE);
 
@@ -13,62 +16,72 @@ const express = require('express');
 const { release } = require('process');
 
 let app = express();
-let http = app.listen(process.env.PORT || 1233);
+let http = app.listen(SITE.port);
 
 app.use('/publish', express.static(path.join(__dirname, DIR_PUBLISH)));
 
 app.get("/*", (req, res) => {
     const domain = req.host;
-    let root = '', file = req.url, pathFile = '';
-    console.log('[2] ', domain, req.method, file);
+    let site, root, file = req.url, ref,
+        pathFile, isDynamic = false,
+        extension = path.extname(file),
+        fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
 
-    if (req.method == 'GET') {
-        if (file.endsWith('favicon.ico')) {
+    if (file == '/clear-cache') {
+        URL_CACHE_TEXT = Object.create({});
+        res.end('CLEAR ALL CACHE ...');
+        return;
+    }
+
+    if (extension.length == 0) {
+        if (file == '/') file = '/index.html';
+        else file += '.html';
+        isDynamic = true;
+        extension = '.html';
+    }
+
+    switch (extension) {
+        case '.ico'://favicon.ico
             res.end();
             return;
-        } else {
-            let site = {
-                dir: 'coming-soon',
-                login: false
-            };
+        case '.html':
+            site = { dir: 'coming-soon', login: false };
             if (SITE.hasOwnProperty(domain)) site = SITE[domain];
             else if (domain.indexOf('login.') == 0) site = SITE['login'];
             root = './' + site.dir;
+            pathFile = root + file;
+            if (!fs.existsSync(pathFile)) pathFile = './404/index.html';
+            URL_STORE_DIR[fullUrl] = site.dir;
+            break;
+        default:
+            ref = req.headers.referer;
+            if (ref && ref.length > 0 && URL_STORE_DIR.hasOwnProperty(ref))
+                pathFile = './' + URL_STORE_DIR[ref] + file;
+            else pathFile = '.' + file;
+            break;
+    }
 
-            switch (file) {
-                case '/test':
-                    res.end();
-                    return;
-                default:
-                    if (file == '/') file = '/index.html';
-                    pathFile = root + file;
-                    if (pathFile.indexOf('?')) pathFile = pathFile.split('?')[0];
+    if (pathFile.indexOf('?')) pathFile = pathFile.split('?')[0];
+    //console.log('[3] ', domain, req.url, pathFile);
 
-                    console.log('[3] ' + pathFile);
-
-                    if (fs.existsSync(pathFile)) {
-                        const mimeType = mime.lookup(pathFile);
-                        if (mimeType == false) {
-                            ;
-                        } else {
-                            res.setHeader('content-type', mimeType);
-                        }
-                        const src = fs.createReadStream(pathFile);
-                        src.pipe(res);
-                    } else {
-                        res.send('Error, Cannot find the file: ' + pathFile);
-                        res.end();
-                    }
-                    break;
+    const mimeType = mime.lookup(pathFile);
+    if (mimeType != false) res.setHeader('content-type', mimeType);
+    if (URL_CACHE_TEXT.hasOwnProperty(fullUrl)) {
+        res.end(URL_CACHE_TEXT[fullUrl]);
+    } else {
+        if (fs.existsSync(pathFile)) {
+            if (extension == '.html' || extension == '.js' || extension == '.css') {
+                const s = fs.readFileSync(pathFile);
+                URL_CACHE_TEXT[fullUrl] = s;
+                res.end(s);
+            } else {
+                const src = fs.createReadStream(pathFile);
+                src.pipe(res);
             }
-
-            //res.send('Hi, Time is ' + (new Date()).toString());
-            //res.end();
-            return;
+        } else {
+            res.end();
         }
     }
-    res.send('Hi, Time is ' + (new Date()).toString());
-    res.end();
 });
 
 
