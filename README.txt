ed26bfdb95d4e14163a73a41dbca3a31d08f1bcb
thinhmascot <67301649+thinhmascot@users.noreply.github.com>
2020-11-13 07:24:31 +0700

    a

--- README.txt
+++ README.txt
@@ -1,3 +1,15 @@
+127.0.0.1        api.lo
+127.0.0.1        file.lo
+127.0.0.1        admin.lo
+127.0.0.1        home.lo
+127.0.0.1        theme.lo
+127.0.0.1        english.lo
+127.0.0.1        task.lo
+127.0.0.1        login.lo
+127.0.0.1        cloud.lo
+127.0.0.1        site-1.lo
+127.0.0.1        site-2.lo
+
 
 npm i lodash mime-types express 
 
--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const DIR_PUBLISH = '_pub';
 
 const URL_STORE_DIR = {};
 let URL_CACHE_TEXT = {}
-let SITE = require('./_sys/domain.' + ENV + '.json');
+let SITE, PAGE_404, API_URL, API_DOMAIN;
+reloadCache();
 //console.log(SITE);
 
 const fs = require('fs');
@@ -13,7 +14,39 @@ const path = require('path');
 const _ = require('lodash');
 const mime = require('mime-types');
 const express = require('express');
-const { release } = require('process');
+
+//------------------------------------------------------------------------------
+
+function reloadCache(res) {
+    const fileSetting = './_sys/domain.' + ENV + '.json';
+    delete require.cache[require.resolve(fileSetting)];
+    SITE = require(fileSetting);
+
+    PAGE_404 = SITE['404'].index;
+    if (PAGE_404 == null || PAGE_404.length == 0) PAGE_404 = './404/index.html';
+
+    API_URL = '';
+    API_DOMAIN = '';
+    if (SITE.url && SITE.url.api) {        
+        const a = SITE.url.api.split('/');
+        if (a.length > 2) {
+            API_URL = SITE.url.api;
+            API_DOMAIN = a[2].split(':')[0].trim();
+        }
+    }
+
+    URL_CACHE_TEXT = Object.create({});
+
+    if (res) {
+        res.end('CLEAR ALL CACHE ...');
+    }
+}
+
+function getFullPath(...paths) {
+    return paths.reduce((a, b) => path.join(a, b), process.cwd());
+}
+
+//------------------------------------------------------------------------------
 
 let app = express();
 let http = app.listen(SITE.port);
@@ -21,20 +54,22 @@ let http = app.listen(SITE.port);
 app.use('/publish', express.static(path.join(__dirname, DIR_PUBLISH)));
 
 app.get("/*", (req, res) => {
-    const domain = req.host;
+    const domain = req.hostname;
     let site, root, file = req.url, ref,
-        pathFile, isDynamic = false,
+        pathFile, isDynamic = false, isHome = false,
         extension = path.extname(file),
         fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
 
     if (file == '/clear-cache') {
-        URL_CACHE_TEXT = Object.create({});
-        res.end('CLEAR ALL CACHE ...');
+        reloadCache(res);
         return;
     }
 
     if (extension.length == 0) {
-        if (file == '/') file = '/index.html';
+        if (file == '/') {
+            isHome = true;
+            file = '/index.html';
+        }
         else file += '.html';
         isDynamic = true;
         extension = '.html';
@@ -45,29 +80,39 @@ app.get("/*", (req, res) => {
             res.end();
             return;
         case '.html':
-            site = { dir: 'coming-soon', login: false };
+            site = { dir: 'coming-soon', login: false, browser: false };
             if (SITE.hasOwnProperty(domain)) site = SITE[domain];
             else if (domain.indexOf('login.') == 0) site = SITE['login'];
-            root = './' + site.dir;
-            pathFile = root + file;
-            if (!fs.existsSync(pathFile)) pathFile = './404/index.html';
+            if (isHome && site.browser) {
+                site = SITE['404'];
+                root = './' + site.dir;
+                pathFile = site.browser;
+            } else {
+                root = './' + site.dir;
+                pathFile = root + file;
+                if (!fs.existsSync(pathFile)) pathFile = PAGE_404;
+            }
             URL_STORE_DIR[fullUrl] = site.dir;
             break;
         default:
             ref = req.headers.referer;
             const dir = URL_STORE_DIR[ref];
-            if (file.indexOf('/' + dir) == -1 &&  ref && ref.length > 0 && URL_STORE_DIR.hasOwnProperty(ref))
+            //console.log('[2.ref] ', dir, file, ref);
+            if (file.indexOf('/' + dir) == -1
+                && file.indexOf('/404/') == -1
+                && ref && ref.length > 0
+                && URL_STORE_DIR.hasOwnProperty(ref))
                 pathFile = './' + dir + file;
             else pathFile = '.' + file;
             break;
     }
 
     if (pathFile.indexOf('?')) pathFile = pathFile.split('?')[0];
-    console.log('[3] ', domain, req.url, pathFile);
+    //console.log('[3] ', domain, req.url, pathFile);
 
     const mimeType = mime.lookup(pathFile);
     if (mimeType != false) res.setHeader('content-type', mimeType);
-    if (URL_CACHE_TEXT.hasOwnProperty(fullUrl)) {
+    if (ENV == 'pro' && URL_CACHE_TEXT.hasOwnProperty(fullUrl)) {
         res.end(URL_CACHE_TEXT[fullUrl]);
     } else {
         if (fs.existsSync(pathFile)) {
@@ -85,11 +130,8 @@ app.get("/*", (req, res) => {
     }
 });
 
+app.post("/*", (req, res) => {
+    const domain = req.hostname;
+    let site, root, file = req.url, ref;
 
-
-
-
-
-
-
-
+});
